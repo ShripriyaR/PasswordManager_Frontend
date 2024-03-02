28e057b1c34993139252c392892700d9460fb024
ShripriyaR <shripriyaravi@gmail.com>
2024-03-02 13:24:53 +0530

    update home1

--- src/component/Home1.jsx
+++ src/component/Home1.jsx
@@ -3,12 +3,15 @@ import AddIcon from '@mui/icons-material/Add';
 import AddAppForm from './AddAppForm';
 import AppCard1 from './AppCard1';
 import {useSelector } from "react-redux";
+import {Link} from 'react-router-dom';
 
 
 function Home1(){
     const user = useSelector(store => store.user);
     return <div>
-        <h4 className= "home-page-header">Welcome {user.username}</h4>
+        <h4 className= "home-page-header">Welcome {user.username}
+        <Link className='flex-end'to='/'>Home</Link>
+        </h4>
         <AddAppForm />
         <div className="container">
             <div className="row row-cols-1 row-cols-md-4 row-cols-sm-3 cards-container">
